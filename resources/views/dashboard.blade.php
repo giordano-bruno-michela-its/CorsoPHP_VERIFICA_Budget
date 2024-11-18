bc1d2d5d57c2ebc2056fde507cf0b7e25b9eb63b
Giordano Bruno Michela <giordanob.m85@gmail.com>
2024-11-18 23:45:39 +0100

    dashboard - add transaction button inside table's div

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,35 +28,18 @@
     </div>
 
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-6">
-        <div class="flex justify-end mb-4">
-
-            <div x-data="{ open: false }" class="relative">
-                <button @click="open = !open" class="bg-blue-500 text-white p-2 rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-                    </svg>
-                </button>
-                <div x-show="open" @click.away="open = false" class="absolute right-0 mt-2 w-48 bg-white border border-gray-200 dark:bg-gray-800 dark:border-gray-500 rounded shadow-lg">
-                    <ul>
-                        <li class="px-4 py-2 border-b border-gray-200 dark:border-gray-500">
-                            <a href="{{ route('transactions.create') }}" class="text-blue-500">Add Transaction</a>
-                        </li>
-                        <li class="px-4 py-2 border-b border-gray-200 dark:border-gray-500">
-                            <a href="{{ route('accounts.create') }}" class="text-green-500">Add Account</a>
-                        </li>
-                        <li class="px-4 py-2">
-                            <a href="{{ route('transaction-types.create') }}" class="text-purple-500">Add Transaction Type</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="p-6 text-gray-900 dark:text-gray-100 relative">
                 <h2 class="mb-6 font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">
                     Transactions
                 </h2>
+                <div class="absolute top-0 right-0 mt-4 mr-7">
+                    <a href="{{ route('transactions.create') }}" class="bg-blue-500 text-white p-2 rounded-full flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+                        </svg>
+                    </a>
+                </div>
                 <div class="overflow-x-auto">
                     <table class="min-w-full divide-y divide-gray-200 w-full" data-sort-dir="asc">
                         <thead>
@@ -97,8 +80,6 @@
                 </div>
             </div>
         </div>
-
-
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
