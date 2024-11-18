3374454fa776bb1fbc4f44981b80795d15ecf289
Giordano Bruno Michela <giordanob.m85@gmail.com>
2024-11-18 10:17:31 +0100

    dashboard js moved in separate file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -162,70 +162,9 @@
 
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script>
-            function sortTable(column, direction) {
-                $.ajax({
-                    url: '{{ route('transactions.sort') }}',
-                    type: 'GET',
-                    data: {
-                        column: column,
-                        direction: direction
-                    },
-                    success: function(data) {
-                        $('#transactionsTable').html(data);
-                        document.getElementById("transactionsTable").setAttribute("data-sort-dir", direction);
-                        updateSortArrows(column, direction);
-                    }
-                });
-            }
-
-            function toggleSortDirection(column) {
-                const currentDirection = document.getElementById("transactionsTable").getAttribute("data-sort-dir");
-                return currentDirection === 'asc' ? 'desc' : 'asc';
-            }
-
-            function updateSortArrows(column, direction) {
-                const arrows = document.querySelectorAll('.sort-arrow');
-                arrows.forEach(arrow => arrow.innerHTML = '&#9650;&#9660;'); // Reset all arrows to up and down
-
-                const arrow = document.getElementById(column + '_arrow');
-                if (direction === 'asc') {
-                    arrow.innerHTML = '&#9650;'; // Up arrow
-                } else {
-                    arrow.innerHTML = '&#9660;'; // Down arrow
-                }
-            }
-
-            document.addEventListener('DOMContentLoaded', function() {
-                document.getElementById("transactionsTable").setAttribute("data-sort-dir", "desc");
-                sortTable('created_at', 'desc');
-            });
-
-            function adjustDate(id, months) {
-                const input = document.getElementById(id);
-                let date = new Date(input.value);
-                date.setMonth(date.getMonth() + months);
-
-                const today = new Date();
-                if (date > today) {
-                    date = today;
-                }
-
-                input.value = date.toISOString().split('T')[0];
-                validateDates();
-            }
-
-            function validateDates() {
-                const startDateInput = document.getElementById('start_date');
-                const endDateInput = document.getElementById('end_date');
-
-                const startDate = new Date(startDateInput.value);
-                const endDate = new Date(endDateInput.value);
-
-                if (endDate < startDate) {
-                    endDateInput.value = startDateInput.value;
-                }
-            }
+            var transactionsSortUrl = "{{ route('transactions.sort') }}";
         </script>
+        <script src="{{ asset('js/dashboard.js') }}"></script>
 
     </div>
 
