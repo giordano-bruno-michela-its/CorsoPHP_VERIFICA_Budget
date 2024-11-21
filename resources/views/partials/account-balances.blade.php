9ddce1d651e9a98c3918318e2cc652f87c66ebbe
Giordano Bruno Michela <giordanob.m85@gmail.com>
2024-11-21 22:12:17 +0100

    trying - account balances layout

--- resources/views/partials/account-balances.blade.php
+++ resources/views/partials/account-balances.blade.php
@@ -21,6 +21,8 @@
         </div>
         <div class="mt-4">
             <h4 class="text-lg font-medium text-gray-900 dark:text-gray-100">Total Balance: {{ number_format($totalBalance, 2) }} &euro;</h4>
+        </div>
+        <div class="pt-6">
             <button onclick="resetFilter()" class="inline-flex items-center px-4 py-2 bg-gray-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-500 active:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Reset</button>
         </div>
     </div>
