bbb4ff8e05894ec0941d5930d2acb19e9ed098d0
Giordano Bruno Michela <giordano.michela@edu.itspiemonte.it>
2024-11-22 11:50:28 +0100

    fix - edit transaction - "download pdf" styled as button

--- resources/views/transactions/edit.blade.php
+++ resources/views/transactions/edit.blade.php
@@ -75,7 +75,7 @@
                     @if(Str::endsWith($transaction->file_path, ['.jpg', '.jpeg', '.png', '.gif', '.svg']))
                     <img src="{{ asset('storage/' . $transaction->file_path) }}" alt="Uploaded Image" class="w-full h-auto">
                     @elseif(Str::endsWith($transaction->file_path, ['.pdf']))
-                    <a href="{{ asset('storage/' . $transaction->file_path) }}" target="_blank" class="text-blue-500 hover:underline mt-4 block">Download PDF</a>
+                    <a href="{{ asset('storage/' . $transaction->file_path) }}" target="_blank" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 mt-4 block">Download PDF</a>
                     @endif
                 </div>
             </div>
